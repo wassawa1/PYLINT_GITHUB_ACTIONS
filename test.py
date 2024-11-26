76c32ef51730ddc593df2a3519e6d5cd9ecfcb31
Wassawa <wwassa737@gmail.com>
2024-11-27 08:33:38 +0900

    initial commit

---
+++ test.py
@@ -0,0 +1,15 @@
+"""これは単なるテストプログラムです。"""
+
+def main():
+    """ただのテスト関数です。
+    """
+    a = 2
+    b = 4
+
+    print(f"{a=}")
+    print(f"{b=}")
+    print(f"{a+b=}")
+    print(f"{a*b-b=}")
+
+if __name__ == "__main__":
+    main()
