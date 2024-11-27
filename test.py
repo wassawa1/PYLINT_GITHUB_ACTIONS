ed43eb7cd1643897741a985d4ca5af72502a41bc
Wassawa <wwassa737@gmail.com>
2024-11-27 09:27:11 +0900

    Pylintの設定を更新し、requirements.txtを追加。テストプログラムのコメントを改善。

--- test.py
+++ test.py
@@ -1,15 +1,22 @@
-"""これは単なるテストプログラムです。"""
+"""pylintの動作を理解してもらうだけの非常に単純なプログラムです。"""
+
+def sayhello():
+    """挨拶をする関数です。
+    """
+    print("Hello")
 
 def main():
-    """ただのテスト関数です。
+    """挨拶をして、変数aとbの値を表示し、a+bとa*b-bの値を表示します。
     """
     a = 2
     b = 4
 
+    sayhello()
     print(f"{a=}")
     print(f"{b=}")
     print(f"{a+b=}")
     print(f"{a*b-b=}")
+
 
 if __name__ == "__main__":
     main()
